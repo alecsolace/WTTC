89c47847fa8ea678982f56502806670e650b08b7
Alexander Aguirre <keevinaguirre@gmail.com>
2022-08-24 21:24:51 +0200

    Removed google credentials from config

--- src/config.ts
+++ src/config.ts
@@ -4,37 +4,9 @@ import { GoogleAuth } from "google-auth-library";
 
 dotenv.config();
 
-const {
-  CLIENT_ID,
-  GUILD_ID,
-  DISCORD_TOKEN,
-  type,
-  project_id,
-  private_key_id,
-  private_key,
-  client_email,
-  client_id,
-  auth_uri,
-  token_uri,
-  auth_provider_x509_cert_url,
-  client_x509_cert_url,
-} = process.env;
+const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN } = process.env;
 
-if (
-  !CLIENT_ID ||
-  !GUILD_ID ||
-  !DISCORD_TOKEN ||
-  !type ||
-  !project_id ||
-  !private_key_id ||
-  !private_key ||
-  !client_email ||
-  !client_id ||
-  !auth_uri ||
-  !token_uri ||
-  !auth_provider_x509_cert_url ||
-  !client_x509_cert_url
-) {
+if (!CLIENT_ID || !GUILD_ID || !DISCORD_TOKEN) {
   throw new Error("Missing enviroment variables");
 }
 
@@ -42,16 +14,6 @@ const config: Record<string, string> = {
   CLIENT_ID,
   GUILD_ID,
   DISCORD_TOKEN,
-  type,
-  project_id,
-  private_key_id,
-  private_key,
-  client_email,
-  client_id,
-  auth_uri,
-  token_uri,
-  auth_provider_x509_cert_url,
-  client_x509_cert_url,
 };
 
 export default config;
