a2c5ad9cebfd91c678e8152d5a7c2dda8809009f
Alexander Aguirre <keevinaguirre@gmail.com>
2022-09-14 20:19:56 +0200

    title change

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -26,14 +26,14 @@ export async function execute(interaction: CommandInteraction, client: Client) {
   }
 
   const embeddedMessage = new MessageEmbed()
-    .setTitle("World Traveling Trading Circus")
+    .setAuthor({ name: "World Traveling Trading Circus" })
     .setColor("AQUA")
-    .setAuthor({ name: "WTTC-Bot Help" })
     .setTimestamp()
-    .setFooter({ text: "Cheers from AlecSolaris - Wisur" })
+    .setTitle("WTTC-Bot Help")
     .setDescription(
       "The bot is synced with the spreadsheet https://bit.ly/wttc_fleet that you can also find over at current-fleet and will pull info from there. Atm only ships owned by members will be shown in the bot, but this will be changed soon(ish) \n \n /brand - Returns a list of ships based on the selected brand \n eg. /brand Crusader \n \n /fleetvalue - Returns the current value of WTTC's combined fleet value, along some other stats \n \n /memberfleet - Returns a list of a members owned ships as per the spreadsheet \n eg. /memberfleet Wisur \n *The membername has to be the same as what's used in the spreadsheet. The names should be identical to their discord nick, but some might have written it differently in the spreadsheet. \n \n /ship - returns information on the selected ship \n eg. /ship 400i \n *For now only ships that a member own will give a callback from the spreadsheet, but this will be changed in a later iteration of the bot. \n \n Disclaimer: This bot is ongoing and still under development, any issue you find will be fixed later, just let us know.. This is the first iteration open to member access, and it will increase in amounts of things it can do as we go along. Every ship in game will eventually be searchable, but we have the same time horizon on things as our beloved God CR, so take from it what you want."
-    );
+    )
+    .setFooter({ text: "Cheers from AlecSolaris - Wisur" });
 
   interaction.reply({ embeds: [embeddedMessage] });
 }
