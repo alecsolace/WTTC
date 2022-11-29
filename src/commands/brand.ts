bedbd76ff2eca69f8abeb9e116565aebe61ef79e
Alexander Aguirre <keevinaguirre@gmail.com>
2022-11-29 23:27:40 +0100

    Autocomplete init

--- src/commands/brand.ts
+++ src/commands/brand.ts
@@ -1,5 +1,6 @@
 import * as discordJs from "discord.js";
 import { SlashCommandBuilder } from "discord.js";
+import * as fs from "fs";
 import { getManufacturers } from "../googleConfig";
 const members: [name: string, value: string][] = Object(
   require("../../members.json")
@@ -70,4 +71,59 @@ export async function execute(
   embeddedMessage.addFields([field]);
 
   await interaction.editReply({ embeds: [embeddedMessage] });
+}
+
+//discord autocomplete function in typescript?
+
+export async function autocomplete(
+  interaction: discordJs.CommandInteraction,
+  client: discordJs.Client
+) {
+  if (!interaction?.channelId) {
+    return;
+  }
+  const channel = await client.channels.fetch(interaction.channelId);
+
+  if (!channel || channel.type !== discordJs.ChannelType.GuildText) {
+    return;
+  }
+
+  const brand = interaction.options.get("brand")!.value! as string;
+  let memberShips: any = fs.readFile(
+    "ships.json",
+    "utf8",
+    async (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      let ships = JSON.parse(data);
+      let ownedShips: any[] = ships.filter((ship: any) =>
+        ship.manufacturer.toLowerCase().includes(brand.toLowerCase())
+      );
+
+      if (memberShips.shipString.length == 0) {
+        await interaction.editReply(`Could not find ships for: ${brand}`);
+        return;
+      }
+      const embeddedMessage = new discordJs.EmbedBuilder()
+        .setTitle(memberShips.manufacturer)
+        .setColor("Aqua")
+        .setAuthor({ name: "WTTC-Bot" })
+        .setTimestamp()
+        .setFooter({ text: "WTTC-Bot" })
+        .setDescription(
+          `The brand ${memberShips.manufacturer} has the following ships: `
+        );
+
+      let field: discordJs.EmbedField = {
+        name: memberShips.manufacturer,
+        value: memberShips.shipString,
+        inline: true,
+      };
+      embeddedMessage.addFields([field]);
+
+      await interaction.editReply({ embeds: [embeddedMessage] });
+    }
+  );
 }
